301df912b83346bee23fa53726bda21b6bc78eef
Aznam YACOUB <az.yacoub@gmail.com>
2020-09-11 14:18:28 +0200

    [workspace] Add workspace header.

---
+++ productive40.hpp
@@ -0,0 +1,67 @@
+/**
+ * \file
+ * \author Aznam Yacoub (aznam.yacoub@lis-lab.fr)
+ * \date Sept. 9 2020
+ * \version 1.0
+ * \brief Workspace Main Include File.
+ * \details This file gathers all the files required to interact with the entire workspace.
+ */
+
+/*
+===================================================================================================
+	Macros
+===================================================================================================
+*/
+#pragma once
+
+/*
+===================================================================================================
+    Code
+===================================================================================================
+*/
+
+/**
+ * \namespace lis
+ * \brief Main LIS namespace.
+ * \details Main LIS namespace.
+ */
+namespace lis {
+	
+	/**
+	 * \namespace pecase
+	 * \brief PECASE namespace.
+	 * \details PECASE namespace.
+	 */
+	namespace pecase {
+
+		/**
+		 * \namespace productive40
+		 * \brief Contains the code related to the workspace PRODUCTIVE 4.0.
+		 * \details Contains the code related to the workspace PRODUCTIVE 4.0.
+		 */
+		namespace productive40 {
+
+		#pragma region Forward Declarations
+
+			// Forward declare here global variables, constants, functions shared by all
+			// the projects of the workspace.
+
+		#pragma endregion
+
+		}; // namespace productive40
+
+	}; // namespace pecase
+
+}; // namespace lis
+
+/*
+===================================================================================================
+	Projects Includes
+===================================================================================================
+*/
+
+/*
+===================================================================================================
+    Macros
+===================================================================================================
+*/
